f672fc5712796946abec4b6f2aa90ebfce35732a
aqaPayam <93112601+aqaPayam@users.noreply.github.com>
2022-04-18 14:27:46 +0430

    Add files via upload

--- src/Model/MainMap.java
+++ src/Model/MainMap.java
@@ -1,10 +1,14 @@
 package Model;
 
+import Model.Terrains.Terrain;
+
 import java.util.ArrayList;
 
 public class MainMap extends Map {
     public void updateMap() {
         //bere GameDatabase.civilization.map ok kone
+        Terrain terrain;
+        terrain.
     }
     public MainMap() {
         //TODO GENERATE MAP
--- src/Model/Terrains/Terrain.java
+++ src/Model/Terrains/Terrain.java
@@ -1,8 +1,11 @@
 package Model.Terrains;
 
 import Model.Buildings.Building;
+import Model.Citizen;
 import Model.Resources.Resource;
 import Model.TerrainFeatures.TerrainFeature;
+import Model.Units.MilitaryUnit;
+import Model.Units.Unit;
 
 import java.util.ArrayList;
 
@@ -13,6 +16,34 @@ public class Terrain implements CitizenCanWork {
     private ArrayList<Resource> resources;
     private Building building;
 
+    public ArrayList<Citizen> getCitizens() {
+        return citizens;
+    }
+
+    public void setCitizens(ArrayList<Citizen> citizens) {
+        this.citizens = citizens;
+    }
+
+    private Unit civilianUnit;
+
+    private ArrayList<Citizen> citizens;
+    private MilitaryUnit militaryUnit;
+
+    public Unit getCivilianUnit() {
+        return civilianUnit;
+    }
+
+    public void setCivilianUnit(Unit civilianUnit) {
+        this.civilianUnit = civilianUnit;
+    }
+
+    public MilitaryUnit getMilitaryUnit() {
+        return militaryUnit;
+    }
+
+    public void setMilitaryUnit(MilitaryUnit militaryUnit) {
+        this.militaryUnit = militaryUnit;
+    }
 
     public TerrainType getType() {
         return type;
@@ -54,12 +85,14 @@ public class Terrain implements CitizenCanWork {
         this.building = building;
     }
 
-    public Terrain(TerrainType type, TerrainState state, ArrayList<TerrainFeature> terrainFeatures, ArrayList<Resource> resources, Building building) {
+    public Terrain(TerrainType type, TerrainState state, ArrayList<TerrainFeature> terrainFeatures, ArrayList<Resource> resources, Building building, Unit civilianUnit, ArrayList<Citizen> citizens, MilitaryUnit militaryUnit) {
         this.type = type;
         this.state = state;
         this.terrainFeatures = terrainFeatures;
         this.resources = resources;
         this.building = building;
+        this.civilianUnit = civilianUnit;
+        this.citizens = citizens;
+        this.militaryUnit = militaryUnit;
     }
-
 }
