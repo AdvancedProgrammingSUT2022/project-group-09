5ec0aea915b7a88c6dfbab463da6eaf27bf7d501
PouyaL03 <pouya.lahabi@gmail.com>
2022-04-22 01:21:28 +0430

    moved show map functions to mapController

--- src/main/java/com/civilization/MenuRegex/GameMenuRegex.java
+++ src/main/java/com/civilization/MenuRegex/GameMenuRegex.java
@@ -10,7 +10,10 @@ public enum GameMenuRegex {
     GOLDCHEAT1("^increase (--gold|-g) (?<amount>-?[\\d]+)$"),
     SHOWCURRENTMENU("^menu show-current$"),
     ENTER("^menu enter (?<menuname>(Profile menu)|(Game menu)|(Main menu)|(Login menu))$"),
-    EXIT("^menu exit$");
+    EXIT("^menu exit$"),
+    SHOWMAP1("^show map (--coordinates|-c) -x (?<x>-?[\\d]+) -y (?<y>-?[\\d]+)$"),
+    SHOWMAP2("^show map (--coordinates|-c) -y (?<y>-?[\\d]+) -x (?<x>-?[\\d]+)$"),
+    SHOWMAP("");
 
     private final String regex;
 
@@ -28,7 +31,26 @@ public enum GameMenuRegex {
         return usernames;
     }
 
+    private static final ArrayList<GameMenuRegex> showmapRegexes = new ArrayList<GameMenuRegex>() {
+        {
+            add(SHOWMAP1);
+            add(SHOWMAP2);
+        }
+    };
+
+    private static Matcher getMatcherShowMap(String input) {
+        for (GameMenuRegex command : showmapRegexes) {
+            Matcher matcher = Pattern.compile(command.regex).matcher(input);
+            if (matcher.matches()) {
+                return matcher;
+            }
+        }
+        return null;
+    }
+
     public static Matcher getMatcher(String input, GameMenuRegex command) {
+        if (command.equals(SHOWMAP)) 
+            getMatcher(input, command);
         Matcher matcher = Pattern.compile(command.regex).matcher(input);
         if (matcher.matches()) {
             return matcher;
--- src/main/java/com/civilization/View/GameMenuView.java
+++ src/main/java/com/civilization/View/GameMenuView.java
@@ -2,16 +2,20 @@ package com.civilization.View;
 
 import java.util.Scanner;
 
+import com.civilization.Controller.GameControllerPackage.GameDataBase;
 import com.civilization.Controller.GameControllerPackage.GameMenuController;
+import com.civilization.Controller.GameControllerPackage.MapController;
 import com.civilization.MenuRegex.GameMenuRegex;
 import com.civilization.MenuRegex.MainMenuRegex;
 
 public class GameMenuView extends View {
     private final GameMenuController gameMenuController;
+    private final MapController mapController;
 
     public GameMenuView(Scanner scanner, GameMenuController gameMenuController) {
         super(scanner);
         this.gameMenuController = gameMenuController;
+        this.mapController = new MapController();
     }
 
     @Override
@@ -21,14 +25,18 @@ public class GameMenuView extends View {
             input = scanner.nextLine();
             if ((matcher = GameMenuRegex.getMatcher(input, GameMenuRegex.EXIT)) != null)
                 System.out.println(gameMenuController.exit());
-             else if ((matcher = GameMenuRegex.getMatcher(input, GameMenuRegex.SHOWCURRENTMENU)) != null)
+            else if ((matcher = GameMenuRegex.getMatcher(input, GameMenuRegex.SHOWCURRENTMENU)) != null)
                 System.out.println(CurrentMenu.get());
-             //TODO
-
-
+            else if ((matcher = GameMenuRegex.getMatcher(input, GameMenuRegex.SHOWMAP)) != null)
+                System.out.print(mapController.showMap(matcher, GameDataBase.getCurrentCivilization().getMap().getTerrains()));
+            // TODO
             else
                 System.out.println("invalid command");
 
         }
+    }
+
+    private void showMenu() {
+
     }
 }
